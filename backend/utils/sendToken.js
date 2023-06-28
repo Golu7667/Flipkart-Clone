75734851e549159ad2589a7f838e137697ccce1b
unknown <goluchoudhary7808@gmail.com>
2023-06-28 11:56:15 +0530

    set_samesite

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -5,6 +5,7 @@ const sendToken = (user, statusCode, res) => {
     const options = {
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 1 day
         httpOnly: true,
+        sameSite: "none",
         secure:true
     }
     const token = jwt.sign({ id: user._id }, 'secret')
