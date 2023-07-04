948e661e02b003491a07fe71e104ddf37ef13223
unknown <goluchoudhary7808@gmail.com>
2023-07-04 17:16:43 +0530

    upadate_manifest

--- frontend/src/urlconfig.js
+++ frontend/src/urlconfig.js
@@ -1,3 +1,3 @@
-const baseurl="https://flipkart-ru60.onrender.com/api/v1"
-// http://localhost:4000/api/v1
+const baseurl="http://localhost:4000/api/v1" 
+// https://flipkart-ru60.onrender.com/api/v1
 export default baseurl;
