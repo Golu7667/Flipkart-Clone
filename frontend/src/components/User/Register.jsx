6e34f6e4dee51ceaa64a6f8c2a6e7194ae187674
unknown <goluchoudhary7808@gmail.com>
2023-07-08 15:14:37 +0530

    done_register_page_res

--- frontend/src/components/User/Register.jsx
+++ frontend/src/components/User/Register.jsx
@@ -98,11 +98,12 @@ const Register = () => {
             <main className="w-full mt-12 sm:pt-20 sm:mt-0">
 
                 {/* <!-- row --> */}
-                <div className="flex sm:w-4/6 sm:mt-4 m-auto mb-7 bg-white shadow-lg">
+                <div className="flex md:w-5/6 sm:mt-4 m-auto mb-7 bg-white shadow-lg">
 
                     <FormSidebar
                         title="Looks like you're new here!"
                         tag="Sign up with your mobile number to get started"
+                        className="sm:hidden"
                     />
 
                     {/* <!-- signup column --> */}
@@ -191,7 +192,7 @@ const Register = () => {
                                             name="avatar"
                                             accept="image/*"
                                             onChange={handleDataChange}
-                                            // className="hidden"
+                                            className="hidden"
                                         />
                                         Choose File
                                     </label>
